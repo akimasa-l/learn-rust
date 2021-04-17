52c6f1edd39d805d9052e7ed9dc6c33d579ecac5
akimasa-l <liumingzheng8@outlook.com>
2021-04-18 00:09:57 +0900

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,5 +2,7 @@ fn main() {
     let mut a = 10;
     println!("a is {}", a);
     a = 100;
-    println!("a is {}", a);
+    println!("a is {}", a / 11);
+    let b = (1, 4, 5);
+    println!("b is {}", b.2);
 }
