682ea24cf96d5adf019c884c9d45779d923d9b3e
akimasa-l <liumingzheng8@outlook.com>
2021-04-18 13:58:07 +0900

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,18 @@
+fn sub() {
+    let x = 5;
+    println!("x = {}", x);
+    let x = x + 1;
+    println!("x = {}", x);
+    let x = x + 3;
+    println!("x = {}", x);
+}
+fn sub_2(){
+    let spaces="    ";
+    println!("spaces are \"{}\"", spaces);
+    let spaces = spaces.len();
+    println!("spaces are {}", spaces);
+}
+const MOD: i32 = 1_000_000_007;
 fn main() {
     let mut a = 10;
     println!("a is {}", a);
@@ -5,4 +20,8 @@ fn main() {
     println!("a is {}", a / 11);
     let b = (1, 4, 5);
     println!("b is {}", b.2);
+    println!("apple");
+    println!("MOD = {}", MOD);
+    sub();
+    sub_2();
 }
