210484334d085d4c961694b9785d1793b24725ef
akimasa-l <liumingzheng8@outlook.com>
2021-04-18 14:47:27 +0900

    型推論すげぇ～～～～～～～

--- another_function/src/main.rs
+++ another_function/src/main.rs
@@ -1,3 +1,21 @@
+fn parse() {
+    println!("function \"parse\" was called.");
+    let x: i64 = "100".parse().unwrap(); //本来だったらxの型を決めるためにxに型を決めてあげなきゃいけないけど、
+    println!("x is {}", x);
+}
 fn main() {
     println!("Hello, world!");
+    func();
+    let x = 100;
+    parse();
+    let y = "600".parse().expect("Not a number!"); //次のfunc_with_argでyがi64として呼ばれているのでyは自動的にi64になる←すごくね！？
+    func_with_arg(x, y);
+}
+fn func() {
+    println!("function \"func\" was called.")
+}
+fn func_with_arg(x: i64, y: i64) {
+    println!("function \"func_with_arg\" was called.");
+    println!("x is {}", x);
+    println!("y is {}", y);
 }
