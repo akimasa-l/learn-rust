adcc48c214813619041b28c6f3496ed41079209e
akimasa-l <liumingzheng8@outlook.com>
2021-04-18 14:20:11 +0900

    Update main.rs

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,3 +1,26 @@
+fn array() {
+    let mut a = [1, 4, 6];
+    a[2]=4;
+    println!("a is {:?}", a);
+    println!("a[0] is {}", a[0]);
+    let b = a[0] + 10;
+    println!("b is {}", b);
+    let index:usize = "10".parse().unwrap();
+    println!("a[index] is {}", a[index]);
+}
+fn double() {
+    let a = 100000.15;
+    println!("a = {}", a);
+    let a: f32 = a;
+    println!("a = {}", a);
+}
 fn main() {
-    println!("Hello, world!");
+    let guess
+    :isize//ここをコメントアウトしてみる
+    = "42".parse()
+    .expect("Not a number!");
+    // println!("Hello, world!");
+    println!("guess is {}", guess);
+    double();
+    array();
 }
