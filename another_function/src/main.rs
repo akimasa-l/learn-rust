73fa9e7134406eff0b342af97eeadeca33c08ead
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-04-19 09:29:00 +0900

    expressionとstatementを理解したかも！！！！！

--- another_function/src/main.rs
+++ another_function/src/main.rs
@@ -10,12 +10,33 @@ fn main() {
     parse();
     let y = "600".parse().expect("Not a number!"); //次のfunc_with_argでyがi64として呼ばれているのでyは自動的にi64になる←すごくね！？
     func_with_arg(x, y);
+    f();
 }
 fn func() {
     println!("function \"func\" was called.")
 }
 fn func_with_arg(x: i64, y: i64) {
     println!("function \"func_with_arg\" was called.");
+    println!("x is {}", x);
+    println!("y is {}", y);
+}
+
+fn five() -> i32 {
+    println!("function \"five\" was called.");
+    5
+}
+fn f() {
+    println!("function five's return is {}", five());
+    fun()
+}
+fn fun() {
+    let x = 5;
+    println!("x is {}", x);
+    let y = {
+        let x = 4;
+        println!("x is {}", x);
+        x + 5
+    };
     println!("x is {}", x);
     println!("y is {}", y);
 }
