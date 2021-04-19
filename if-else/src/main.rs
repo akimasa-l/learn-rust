7e69d37046c6a6acb132fca70425c6167d533020
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-04-19 09:40:10 +0900

    if文を理解したかも！！！！

--- if-else/src/main.rs
+++ if-else/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed toread line");
+    //println!("Hello, world!");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!(
+        "return is {}",
+        if guess < 54 {
+            println!("guess is lower than 54");
+            54
+        } else {
+            println!("guess is larger or same to 54");
+            43
+        }
+    )
 }
