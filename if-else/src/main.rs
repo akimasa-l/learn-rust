eb90cba0835bfff45a30bc76eea4e11b12f8983f
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-04-21 10:38:30 +0900

    Update main.rs

--- if-else/src/main.rs
+++ if-else/src/main.rs
@@ -3,18 +3,18 @@ fn main() {
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed toread line");
+        .expect("Failed to read line");
     //println!("Hello, world!");
 
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!(
         "return is {}",
-        if guess < 54 {
+        {let ans=if guess < 54 {
             println!("guess is lower than 54");
             54
         } else {
             println!("guess is larger or same to 54");
             43
-        }
+        };ans}
     )
 }
