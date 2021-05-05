ca6988bf6e7886b070947181957456a7d60817b6
akimasa-l <liumingzheng8@outlook.com>
2021-05-05 20:20:41 +0900

    flushなんもわからん

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,9 +1,11 @@
 use proconio::input;
+use std::io::{stdout};
 fn main() {
     loop {
         input! {
             n:i32
         };
+        stdout().flush().unwrap();
         println!("n is {}", n);
     }
 }
