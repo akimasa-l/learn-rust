8e77c660f66da5a2fadf3a725d02050d3d1c1551
akimasa-l <liumingzheng8@outlook.com>
2021-05-21 20:49:04 +0900

    formatつよい

--- what-is-ownership/src/main.rs
+++ what-is-ownership/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut  message = String::from("Hello");
+    let mut message = String::from("Hello");
     println!("message is: {}", message);
     message.push_str(", world!");
     println!("message is: {}", message)
