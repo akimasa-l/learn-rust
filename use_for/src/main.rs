af96b98afb768dc489ae06ef955b3aaf76c4ac5c
akimasa-l <liumingzheng8@outlook.com>
2021-05-21 20:15:45 +0900

    Rangeつよい

--- use_for/src/main.rs
+++ use_for/src/main.rs
@@ -3,4 +3,11 @@ fn main() {
     for i in a.iter() {
         println!("I like {}!", i);
     }
+    countdown()
+}
+fn countdown(){
+    for i in (0..5).rev(){
+        println!("{}!", i);
+    }
+    println!("liftoff!")
 }
