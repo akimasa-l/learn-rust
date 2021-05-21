5e25d8b44e337839df2e81d15e6215be5d463fde
akimasa-l <liumingzheng8@outlook.com>
2021-05-21 20:48:30 +0900

    Stringすげぇ～～～

--- what-is-ownership/src/main.rs
+++ what-is-ownership/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut  message = String::from("Hello");
+    println!("message is: {}", message);
+    message.push_str(", world!");
+    println!("message is: {}", message)
 }
