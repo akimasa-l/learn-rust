e3eb877d50167dafa53117c08cbe4e5e95da3168
akimasa-l <liumingzheng8@outlook.com>
2021-05-21 22:00:27 +0900

    Copyとmoveを理解したぞ！w

--- what-is-ownership/src/main.rs
+++ what-is-ownership/src/main.rs
@@ -2,5 +2,16 @@ fn main() {
     let mut message = String::from("Hello");
     println!("message is: {}", message);
     message.push_str(", world!");
-    println!("message is: {}", message)
+    println!("message is: {}", message);
+    let new_message = message;
+    println!("new message is: {}", new_message);
+    // println!("message is: {}",message);
+    // コンパイルエラー
+    try_clone()
+}
+fn try_clone(){
+    let a=String::from("apple");
+    println!("a is: {}", a);
+    let b=a.clone();
+    println!("a, b is : {}, {}",a,b)
 }
