4e2eddd66b361a2e30a7aaf0fa785035683e5bd5
akimasa-l <liumingzheng8@outlook.com>
2021-05-21 20:11:44 +0900

    cargo fmtやっぱすごいっすね

--- use_for/src/main.rs
+++ use_for/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let a=["apples","grapes","oranges"];
+    let a = ["apples", "grapes", "oranges"];
     for i in a.iter() {
-        println!("I like {}!",i);
+        println!("I like {}!", i);
     }
 }
