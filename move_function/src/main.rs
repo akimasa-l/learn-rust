81ce76dbb292e52b62e2e55f65362a2a607c5acf
akimasa-l <liumingzheng8@outlook.com>
2021-05-22 10:53:48 +0900

    所有権完全に理解したぞ！w

--- move_function/src/main.rs
+++ move_function/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
     let s = String::from("orange");
     println!("s is {}", s);
-    call_for_String(s);
+    call_for_string(s);
     //println!("s is {}",s);
+    //コンパイルエラー
+
+    let t= String::from("apple");
+    println!("t is {}",t);
+    call_for_string(t.clone());
+    println!("t is {}",t)
 }
-fn call_for_String(s: String) {
-    println!("function call_for_String was called.");
+fn call_for_string(s: String) {
+    println!("function call_for_string was called.");
     println!("s is {}", s);
 }
