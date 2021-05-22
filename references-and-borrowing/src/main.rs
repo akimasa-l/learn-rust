b2ef1124e015a8e1265db1c69dbd6a1525ef58e4
akimasa-l <liumingzheng8@outlook.com>
2021-05-22 12:27:34 +0900

    参照完全に理解した

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("apple");
+    println!("s is {}", s);
+    func(&s);
+    println!("s is {}", s);
+}
+fn func(s: &String) {
+    println!("function \"func\" was called.");
+    println!("length of s is {}", s.len());
 }
