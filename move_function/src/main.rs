35468627777be91d8a376e705cb4b2daaa7afa62
akimasa-l <liumingzheng8@outlook.com>
2021-05-22 10:58:43 +0900

    Copy型完全に理解したぞ！w

--- move_function/src/main.rs
+++ move_function/src/main.rs
@@ -5,12 +5,22 @@ fn main() {
     //println!("s is {}",s);
     //コンパイルエラー
 
-    let t= String::from("apple");
-    println!("t is {}",t);
+    let t = String::from("apple");
+    println!("t is {}", t);
     call_for_string(t.clone());
-    println!("t is {}",t)
+    println!("t is {}", t);
+
+    let i = 100;
+    println!("i is {}", i);
+    makes_copy(i);
+    makes_copy(i.clone());
+    println!("i is {}", i);
 }
 fn call_for_string(s: String) {
     println!("function call_for_string was called.");
     println!("s is {}", s);
+}
+fn makes_copy(i: i32) {
+    println!("function make_copy was called.");
+    println!("i is {}", i);
 }
