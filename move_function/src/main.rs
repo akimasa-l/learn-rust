8da95656e6c5299dadad07f7653d00a0f08c42ef
akimasa-l <liumingzheng8@outlook.com>
2021-05-22 10:43:18 +0900

    Update main.rs

--- move_function/src/main.rs
+++ move_function/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("orange");
+    println!("s is {}", s);
+    call_for_String(s);
+    //println!("s is {}",s);
+}
+fn call_for_String(s: String) {
+    println!("function call_for_String was called.");
+    println!("s is {}", s);
 }
