b173c2ce9d20bf15cbc79e855876cf206bbb0891
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-27 08:43:59 +0900

    これが&mutパワーか.....

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    let s = String::from("apple");
+    let mut s = String::from("apple");
     println!("s is {}", s);
     func(&s);
+    println!("s is {}", s);
+    change(&mut s);
     println!("s is {}", s);
 }
 fn func(s: &String) {
     println!("function \"func\" was called.");
     println!("length of s is {}", s.len());
+}
+fn change(s: &mut String) {
+    s.push_str(" aa")
 }
