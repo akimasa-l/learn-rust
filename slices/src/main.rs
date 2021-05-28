8c61987f38b42259d9b51ff3638795a451632daf
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-28 12:52:48 +0900

    slice完全に理解した

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello, world!");
+    println!("s is {}", s);
+    let slice = &s[0..5];
+    println!("slice is {}", slice);
+
+    let slice = &s[1..10];
+    println!("slice is {}", slice);
+    let slice = &s[1..];
+    println!("slice is {}", slice);
+
+    let slice = &s[..10];
+    println!("slice is {}", slice);
+
+    let slice = &s[..];
+    println!("slice is {}", slice);
+
+    println!("first word is {}", first_word(&s));
+}
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &items) in bytes.iter().enumerate() {
+        if items == b' ' {
+            return &s[..i];
+        }
+    }
+    return &s[..];
 }
