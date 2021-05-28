127f6b0d28cdae72387ca3bf6acc324747e38e41
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-28 13:13:54 +0900

    struct完全に理解した

--- defining-structs/src/main.rs
+++ defining-structs/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        mail: String::from("user@example.com"),
+        name: String::from("usr"),
+        age: 10,
+    };
+    println!("name is {}", user.name);
+    println!("mail is {}", user.mail);
+    println!("age is {}", user.age);
+
+    let u = make_user(String::from("u@example.com"), 18);
+    println!("name is {}", u.name);
+    println!("mail is {}", u.mail);
+    println!("age is {}", u.age);
+}
+struct User {
+    mail: String,
+    age: i8,
+    name: String,
+}
+fn make_user(mail_ad: String, age: i8) -> User {
+    User {
+        mail: mail_ad,
+        age,
+        name: String::from("user100"),
+    }
 }
