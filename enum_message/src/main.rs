79ca8076b49e165743861fd4a8fb5ee639927edf
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-29 12:27:59 +0900

    struct sugee

--- enum_message/src/main.rs
+++ enum_message/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    let m = Message::Write(String::from("apple"));
+    m.call()
+}
+impl Message {
+    fn call(&self) {
+        println!("function was called.")
+    }
+}
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(u8, u8, u8),
 }
