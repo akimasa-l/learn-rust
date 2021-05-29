1c43133b73745a6c6ed91403be859ad9306c732a
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-29 12:02:57 +0900

    これがenumか............

--- try_enum/src/main.rs
+++ try_enum/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let four=IPAddrKind::V4;
+    let six=IPAddrKind::V6;
+    println!("{:?} {:?}",four,six)
+}
+#[derive(Debug)]
+enum IPAddrKind{
+    V4,V6
 }
