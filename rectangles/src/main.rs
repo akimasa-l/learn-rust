2f97d194baa8af9fcc6728e9ec659d9b2320947b
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-29 10:47:03 +0900

    struct神！！！！！！

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let r = Rectangle {
+        width: 50,
+        height: 30,
+    };
+    println!("r's width is {} and r's height is {}", r.width, r.height);
+    println!("area is {}", area(&r));
+}
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+fn area(r: &Rectangle) -> u32 {
+    r.width * r.height
 }
