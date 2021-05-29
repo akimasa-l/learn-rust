5b403db772dfdb90ece01dd36d945bc08a662648
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-05-29 12:05:33 +0900

    enumすげぇ〜〜〜〜

--- try_enum/src/main.rs
+++ try_enum/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
     println!("Hello, world!");
-    let four=IPAddrKind::V4;
-    let six=IPAddrKind::V6;
-    println!("{:?} {:?}",four,six)
+    let four = IPAddrKind::V4;
+    let six = IPAddrKind::V6;
+    kind(four);
+    kind(six)
 }
 #[derive(Debug)]
-enum IPAddrKind{
-    V4,V6
+enum IPAddrKind {
+    V4,
+    V6,
+}
+fn kind(k: IPAddrKind) {
+    println!("{:?}", k)
 }
