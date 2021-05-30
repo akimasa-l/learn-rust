db08042f3f05133b418db47b3f345358976890de
akimasa-l <liumingzheng8@outlook.com>
2021-05-30 17:41:41 +0900

    getメゾットすげぇ～～～

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     let mut l=vec![2,3,5];
     println!("first element is {}",l[0]);
     l.push(1);
+    let second:Option<&i32>=l.get(/* 2 */ 100);
+    if let Some(x) = second{
+        println!("second element is {}",x);
+    }else{
+        println!("second element doesn't exist");
+    }
 }
