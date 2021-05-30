3e2c1587f8d3ee803e61f287d38c5c1b411c7288
akimasa-l <liumingzheng8@outlook.com>
2021-05-30 17:42:06 +0900

    formatパワーなめんなよ～～～～～

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let mut l=vec![2,3,5];
-    println!("first element is {}",l[0]);
+    let mut l = vec![2, 3, 5];
+    println!("first element is {}", l[0]);
     l.push(1);
-    let second:Option<&i32>=l.get(/* 2 */ 100);
-    if let Some(x) = second{
-        println!("second element is {}",x);
-    }else{
+    let second: Option<&i32> = l.get(/* 2 */ 100);
+    if let Some(x) = second {
+        println!("second element is {}", x);
+    } else {
         println!("second element doesn't exist");
     }
 }
