4687d8fd098936a8018ec217b296fe70ed7aff57
akimasa-l <liumingzheng8@outlook.com>
2021-05-30 16:08:16 +0900

    相対パス絶対パス完全に理解した

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,11 +1,20 @@
 mod front_of_house {
     mod hosting {
         fn add_to_list() {}
-        fn seat_at_table() {}
+        fn seat_at_table() {
+            super::super::func(); //相対パス
+            super::f(); //相対パス
+        }
+    }
+    fn f() {
+        serving::take_order(); //相対パス
     }
     mod serving {
-        fn take_order() {}
+        pub fn take_order() {}
         fn serve_order() {}
-        fn take_payment() {}
+        fn take_payment() {
+            crate::front_of_house::serving::take_order() //絶対パス
+        }
     }
 }
+fn func() {}
