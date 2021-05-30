94e6bc6f7d79a5e41074e59ee76e1ceedd8111ad
akimasa-l <liumingzheng8@outlook.com>
2021-05-30 12:44:51 +0900

    Option完全に理解した

--- plus_one/src/main.rs
+++ plus_one/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let x=Some(6);
+    plus_one(x);
+    plus_one(plus_one(x));
+    let y=None;
+    plus_one(y);
+    plus_one(plus_one(y));
+}
+fn plus_one(x:Option<u8>)->Option<u8> {
+    match x{
+        Some(x) => {println!("x is: {}",x);Some(x+1)},
+        None => { println!("x is None");None}
+    }
 }
