5699479c110c21a0dac146c7d1a76e11f4e30dd5
akimasa-l <liumingzheng8@outlook.com>
2021-05-30 12:33:03 +0900

    少しわかった気がしますねぇ～～～

--- coins/src/main.rs
+++ coins/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let coin=Coin::Penny;
+    value_in_cent(coin);
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+fn value_in_cent(coin: Coin) -> u32 {
+    match coin {
+        Penny => {
+            println!("Lucky Penny!");
+            1
+        }
+        Nickel => 5,
+        Dime => 10,
+        Quarter => 25,
+    }
 }
