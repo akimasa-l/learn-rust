289897b7f2443b057ee1a6fb0e641d2fb229eadf
akimasa-l <liumingzheng8@outlook.com>
2021-05-30 12:45:06 +0900

    match文天才

--- plus_one/src/main.rs
+++ plus_one/src/main.rs
@@ -1,14 +1,20 @@
 fn main() {
-    let x=Some(6);
+    let x = Some(6);
     plus_one(x);
     plus_one(plus_one(x));
-    let y=None;
+    let y = None;
     plus_one(y);
     plus_one(plus_one(y));
 }
-fn plus_one(x:Option<u8>)->Option<u8> {
-    match x{
-        Some(x) => {println!("x is: {}",x);Some(x+1)},
-        None => { println!("x is None");None}
+fn plus_one(x: Option<u8>) -> Option<u8> {
+    match x {
+        Some(x) => {
+            println!("x is: {}", x);
+            Some(x + 1)
+        }
+        None => {
+            println!("x is None");
+            None
+        }
     }
 }
