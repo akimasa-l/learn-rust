10604b4dfc864c1f83aed317f7c6468cc7751de0
liumingzheng chromebook <61229liu@seiko.ac.jp>
2021-06-25 14:32:00 +0900

    proconioすげぇ〜〜〜〜

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -1,12 +1,31 @@
+use proconio::input;
 use std::collections::HashMap;
 fn main() {
     // println!("Hello, world!");
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-    let blue = scores.get("Blue");
-    match blue {
-        Some(x) => println!("scores Blue: {}", x),
-        None => println!("scores Blue is none"),
+    let mut map = HashMap::new();
+    map.insert(String::from("init"), 0);
+    loop{
+    input! {
+        t:i32,
+        s:String,
+        a:i32,
     }
+    match t {
+        0 => {
+            map.insert(s, a);
+        }
+
+        1 => {
+            let result = map.get(&s);
+            match result {
+                Some(x) => println!("map result: {} is {}",&s, x),
+                None => println!("map result is none"),
+            };
+        }
+        _ => {
+            println!("Please type \"insert\" or \"get\"");
+            break;
+        }
+    }
+}
 }
