0a50fdce6bc68a33e2b94333ff64dd90ae91fc7c
liumingzheng chromebook <61229liu@seiko.ac.jp>
2021-06-25 14:16:36 +0900

    format忘れがち

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -3,11 +3,10 @@ fn main() {
     // println!("Hello, world!");
     let mut scores = HashMap::new();
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"),50);
-    let blue=scores.get("Blue");
-    match blue{
+    scores.insert(String::from("Yellow"), 50);
+    let blue = scores.get("Blue");
+    match blue {
         Some(x) => println!("scores Blue: {}", x),
-        None=> println!("scores Blue is none")
+        None => println!("scores Blue is none"),
     }
-    
 }
